7b654bead13f6a70c1466dd82dcbf94d80e6dc62
Padam Shankhadev <shankhadev123@gmail.com>
2015-12-09 19:48:01 +0545

    date modified

--- npCalender.js
+++ npCalender.js
@@ -2,9 +2,9 @@
 Name:Jquery Nepali Calendar
 Author:Padam Shankhadev
 Creadted Date: 2012-6-17
-Updated Date: 2015-10-2
+Updated Date: 2015-12-09
 */
-(function($){ 
+(function($){
 var bs = {
   0:[2000,30,32,31,32,31,30,30,30,29,30,29,31],
 	1:[2001,31,31,32,31,31,31,30,29,30,29,30,30],
@@ -40,7 +40,7 @@ var bs = {
 	31:[2031,30,32,31,32,31,30,30,30,29,30,29,31],
 	32:[2032,31,31,32,31,31,31,30,29,30,29,30,30],
 	33:[2033,31,31,32,32,31,30,30,29,30,29,30,30],
-	34:[2034,31,32,31,32,31,30,30,30,29,29,30,31], 
+	34:[2034,31,32,31,32,31,30,30,30,29,29,30,31],
 	35:[2035,30,32,31,32,31,31,29,30,30,29,29,31],
 	36:[2036,31,31,32,31,31,31,30,29,30,29,30,30],
 	37:[2037,31,31,32,32,31,30,30,29,30,29,30,30],
@@ -98,27 +98,27 @@ var bs = {
 	89:[2089, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30],
 	90:[2090, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30]
 };
-	
-	
+
+
 	$.fn.npCalendar = function(options){
-		
+
 		var date = new Date();
 		var np_year = date.getUTCFullYear();
 		var np_month = parseInt(date.getUTCMonth()+1);
 		var np_day = date.getUTCDate();
-		
+
 		var defaults = {
 			color:'#000',
 			font:'10px Arial, Helvetica, sans-serif'
 		};
-		
+
 		var option = $.extend({},defaults,options);
 		$(this).each(function(){
 			var nep_date = $.eng_to_nep(np_year,np_month,np_day);
 			$(this).append('<span style="color:'+option.color+ '; font:'+option.font+'">'+nep_date+'</span>');
 		});
 	};
-	
+
 	$.is_leap_year = function(year){
 		var a = year;
 		if (a%100==0)
@@ -129,8 +129,8 @@ var bs = {
 			} else {
 				return false;
 		}
-			
-		} else { 
+
+		} else {
 			if (a%4==0)
 			{
 				return true;
@@ -139,115 +139,115 @@ var bs = {
 			}
 		}
 	};
-	
-	
+
+
 	$.get_nepali_month = function(m){
 		var n_month = false;
-			
+
 			switch(m){
 				case 1:
 					n_month = "बैशाख"; //Baisakh
 					break;
-					
+
 				case 2:
 					n_month = "जेष्ठ"; //Jestha
 					break;
-					
+
 				case 3:
 					n_month = "असार"; //Ashad
 					break;
-					
+
 				case 4:
 					n_month = "श्रावण"; //Shrawn
 					break;
-					
+
 				case 5:
 					n_month = "भाद्र"; //Bhadra
 					break;
-				
+
 				case 6:
 					n_month = "आश्विन"; //Ashoj
 					break;
-				
+
 				case 7:
 					n_month = "कार्तिक"; //kartik
 					break;
-				
+
 				case 8:
 					n_month = "मंसिर"; //Mangshir
 					break;
-				
+
 				case 9:
 					n_month = "पुष"; //Poush
 					break;
-				
+
 				case 10:
 					n_month = "माघ"; //Magh
 					break;
-				
+
 				case 11:
 					n_month = "फाल्गुन"; //Falgun
 					break;
-				
+
 				case 12:
 					n_month = "चैत्र"; //Chaitra
 					break;
-			}	
+			}
 			return  n_month;
 	};
-	
+
 	$.get_day_of_week = function(day){
 		switch(day){
 				case 1:
 					day = "आईतवार"; //Sunday
 					break;
-					
+
 				case 2:
 					day = "सोमबार"; //Monday
 					break;
-					
+
 				case 3:
 					day = "मंगलवार"; //Tuesday
 					break;
-				
+
 				case 4:
 					day = "बुधबार"; //Wednesday
 					break;
-				
+
 				case 5:
 					day = "बिहीबार"; //Thursday
 					break;
-				
+
 				case 6:
 					day = "शुक्रबार"; //Friday
 					break;
-				
+
 				case 7:
 					day = "शनिबार "; //Saturday
 					break;
 			}
 			return day;
 	};
-	
+
 	$.is_range_eng = function(yy, mm, dd){
 		if(yy<1944 || yy>2033){
 				debug_info = "Supported only between 1944-2022";
 				return false;
 			}
-				
+
 			if(mm<1 || mm >12){
 				debug_info = "Error! value 1-12 only";
 				return false;
 			}
-				
+
 			if(dd<1 || dd >31){
-				debug_info = "Error! value 1-31 only";			
+				debug_info = "Error! value 1-31 only";
 				return false;
-			}	
-			
+			}
+
 			return true;
 	};
-	
+
 	$.eng_to_nep = function(yy,mm,dd){
 		if ($.is_range_eng(yy, mm, dd) == false){
 				return false;
@@ -255,13 +255,13 @@ var bs = {
 				// english month data.
 			 	month = [31,28,31,30,31,30,31,31,30,31,30,31];
 			 	lmonth = [31,29,31,30,31,30,31,31,30,31,30,31];
-				
+
 				def_eyy = 1944;									//spear head english date...
 				def_nyy = 2000; def_nmm = 9; def_ndd = 17-1;		//spear head nepali date...
 				total_eDays=0; total_nDays=0; a=0; day=7-1;		//all the initializations...
 				m = 0; y = 0; i =0; j = 0;
 				numDay=0;
-				
+
 				// count total no. of days in-terms of year
 				for(i=0; i<(yy-def_eyy); i++){	//total days for month calculation...(english)
 					if($.is_leap_year(def_eyy+i)==1)
@@ -271,28 +271,28 @@ var bs = {
 						for(j=0; j<12; j++)
 							total_eDays += month[j];
 				}
-				
-				// count total no. of days in-terms of month					
-				for(i=0; i<(mm-1); i++){		
+
+				// count total no. of days in-terms of month
+				for(i=0; i<(mm-1); i++){
 					if($.is_leap_year(yy)==1)
 						total_eDays += lmonth[i];
 					else
 						total_eDays += month[i];
 				}
-				
+
 				// count total no. of days in-terms of date
 				total_eDays += dd;
-				
-				
-				i = 0; j = def_nmm;					
+
+
+				i = 0; j = def_nmm;
 				total_nDays = def_ndd;
 				m = def_nmm;
 				y = def_nyy;
-				
+
 				//alert(m+':'+y+':'+j+':'+day)
 				//alert(bs[69][10]);
 				// count nepali date from array
-				
+
 				while(total_eDays!=0) {
 			        a = bs[i][j];
 					total_nDays++;						//count the days
@@ -314,10 +314,10 @@ var bs = {
 					total_eDays--;
 				}
 				numDay=day;
-				
+
 				String.prototype.replaceArray = function(find, replace) {
 				var replaceString = this;
-				var regex; 
+				var regex;
 				for (var i = 0; i < find.length; i++) {
 					regex = new RegExp(find[i], "g");
 					replaceString = replaceString.replace(regex, replace[i]);
@@ -330,7 +330,7 @@ var bs = {
 				np_month = $.get_nepali_month(m);
 				np_day = $.get_day_of_week(day);
 				np_date = String(total_nDays).replaceArray(find,replace);
-				
+
 				return np_day+','+np_year+' '+np_month+' '+np_date;
 			}
 	};
